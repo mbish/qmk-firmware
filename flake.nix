42251d13ce6931c37555190f820454dc0bb8443c
Mark Bishop <mbishop@duosecurity.com>
2025-10-23 15:42:34 -0400

    add flake description

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "A very basic flake";
+  description = "A Nix flake for building QMK firmware for custom keyboards with specific keymaps.";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
